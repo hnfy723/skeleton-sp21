8ec230a062048937857edc1bd387bb5860aa5f2e
hnfy723 <2746521979@qq.com>
2026-09-20 00:14:57 +0800

    Complete lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -54,7 +54,7 @@ public class IntListExercises {
         while (x > 10) {
             x = x / 10;
         }
-        int firstDigit = x % 10;
+        int firstDigit = x ;
         return firstDigit == lastDigit;
     }
 
@@ -72,11 +72,10 @@ public class IntListExercises {
         }
 
         boolean currElemIsPrime = Primes.isPrime(lst.first);
-
         if (currElemIsPrime) {
             lst.first *= lst.first;
         }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
+        boolean restChanged = squarePrimes(lst.rest);
+        return currElemIsPrime || restChanged;
     }
 }
